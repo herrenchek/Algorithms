72c42e2b859795589273d64beecd2599a83752cd
herrenchek <giselle.herr@icloud.com>
2019-10-20 20:21:03 -0400

    Recursive solution

--- Fibonacci/fibonacci.js
+++ Fibonacci/fibonacci.js
@@ -13,23 +13,23 @@ function fibonacci(nth) {
     return nth >= 1 ? nums[1] : nums[0];
 }
 
-console.log(fibonacci(7)); // 8
+console.log(fibonacci(7)); // 13
 console.log(fibonacci(0)); // 0
 console.log(fibonacci(1)); // 1
-console.log(fibonacci(5)); // 3
+console.log(fibonacci(5)); // 5
 console.log("\n");
 
 
 // Recursively.
 const nthRecursive = nth => {
 // A recursive function is a function that calls itself during its execution.
-    // Base cases that prevent the function from looping infinitely.
+    // Base cases prevent the function from looping infinitely.
     if (nth === 0) return 0;
     if (nth === 1) return 1;
     if (nth > 1) return nthRecursive(nth - 2) + nthRecursive(nth - 1);
 }
 
-console.log(nthRecursive(7)); // 8
+console.log(nthRecursive(7)); // 13
 console.log(nthRecursive(0)); // 0
 console.log(nthRecursive(1)); // 1
-console.log(nthRecursive(5)); // 3
+console.log(nthRecursive(5)); // 5
